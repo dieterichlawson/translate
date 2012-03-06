fb873ad70a0a623a291c684d60d8291a68d6c1f2
Dieterich Lawson <dieterich.lawson@gmail.com>
2012-03-06 12:17:01 -0800

    Changed ordering of fns

--- translate.py
+++ translate.py
@@ -5,6 +5,18 @@ import re
 dictionary = defaultdict(list)
 text = []
 
+def load_text(filename):
+  sentence_delim = '(\?|\.)\s?'
+  textfile = open(filename)
+  for line in textfile:
+    sentences = re.split(sentence_delim,line)
+    for sentence, delim in zip(sentences[::2],sentences[1::2]):
+      words = sentence.split(' ')
+      if len(words) == 1 and words[0] == '\n': continue
+      words.append(delim[0])
+      text.append(words)
+  textfile.close()
+
 def load_dict(filename):
   dictfile = open(filename)
   for line in dictfile:
@@ -23,18 +35,6 @@ def get_definitions(word,pos):
     return dictionary[word+'_'+pos]
   else:
     return dictionary[word]
-
-def load_text(filename):
-  sentence_delim = '(\?|\.)\s?'
-  textfile = open(filename)
-  for line in textfile:
-    sentences = re.split(sentence_delim,line)
-    for sentence, delim in zip(sentences[::2],sentences[1::2]):
-      words = sentence.split(' ')
-      if len(words) == 1 and words[0] == '\n': continue
-      words.append(delim[0])
-      text.append(words)
-  textfile.close()
 
 load_text('text.txt')
 load_dict('dict.txt')
